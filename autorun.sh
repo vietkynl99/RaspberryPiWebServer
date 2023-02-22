b74b96b03613015bfa4281ee384e8cf56b062eb9
vietkynl99 <vietdohoang1999@gmail.com>
2023-02-22 23:25:31 +0700

    Edit autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
 HTML_FOLDER=/var/www/html
+WARNING=n
+CREATE_DATABASE=n
+COPY_TO_HTML_FOLDER=y
+BUILD_SERVER=n
+START_SERVER=n
 
-echo -e "Warning! All data in ${HTML_FOLDER} will be deleted!"
-read -p "Do you want to continue (y/n)? " ans
 
-if test "$ans" != "y" ; then
-    exit
+if test "$WARNING" == "y" ; then
+    echo -e "Warning! All data in ${HTML_FOLDER} will be deleted!"
+    read -p "Do you want to continue (y/n)? " ans
+    if test "$ans" != "y" ; then
+        exit
+    fi 
+fi
+
+if test "$CREATE_DATABASE" == "y" ; then
+    echo -e "Make server database...\n"
+    php resource/sql/createDatabase.php
+fi
+
+if test "$COPY_TO_HTML_FOLDER" == "y" ; then
+    echo -e "\nCopying html resource to ${HTML_FOLDER}..."
+    rm -rf ${HTML_FOLDER}/*
+    cp -rf html/* ${HTML_FOLDER}/
+fi
+
+if test "$BUILD_SERVER" == "y" ; then
+    echo -e "Building server...\n"
+    cd ./resource/server
+    make
 fi 
 
-# echo -e "Make server database...\n"
-# php resource/sql/createDatabase.php
-
-echo -e "\nCopying html resource to ${HTML_FOLDER}..."
-rm -rf ${HTML_FOLDER}/*
-cp -rf html/* ${HTML_FOLDER}/
-
-echo -e "Building server...\n"
-cd ./resource/server
-make
-
-echo -e "\nStart server..."
-./server
-cd ../..
+if test "$START_SERVER" == "y" ; then
+    echo -e "\nStart server..."
+    ./server
+    cd ../..
+fi 
