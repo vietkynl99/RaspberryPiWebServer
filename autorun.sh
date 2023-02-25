443a09e6dd4152a7e52bc31438f771a8e8214f3d
vietkynl99 <vietdohoang1999@gmail.com>
2023-02-25 18:05:22 +0700

    Modify autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,40 +1,62 @@
 #!/bin/bash
 
 HTML_FOLDER=/var/www/html
-WARNING=n
-CREATE_DATABASE=n
-COPY_TO_HTML_FOLDER=y
-BUILD_SERVER=n
-START_SERVER=n
+SERVER_FOLDER=./resource/server
 
-
-if test "$WARNING" == "y" ; then
+if test "$#" == "0" ; then
+    echo -e "Copying html resource to ${HTML_FOLDER}..."
     echo -e "Warning! All data in ${HTML_FOLDER} will be deleted!"
     read -p "Do you want to continue (y/n)? " ans
     if test "$ans" != "y" ; then
         exit
     fi 
-fi
-
-if test "$CREATE_DATABASE" == "y" ; then
-    echo -e "Make server database...\n"
-    php html/home/sql/createDatabase.php
-fi
-
-if test "$COPY_TO_HTML_FOLDER" == "y" ; then
-    echo -e "\nCopying html resource to ${HTML_FOLDER}..."
     rm -rf ${HTML_FOLDER}/*
     cp -rf html/* ${HTML_FOLDER}/
 fi
 
-if test "$BUILD_SERVER" == "y" ; then
-    echo -e "Building server...\n"
-    cd ./resource/server
-    make
-fi 
+if test "$#" == "1" ; then
+    case $1 in
+        -createdb)
+            echo -e "Create server database...\n"
+            php html/home/sql/createDatabase.php
+            exit
+            ;;
+        -startserver)
+            if [ ! -f ${SERVER_FOLDER}/server ] ; then
+                echo -e "Building server...\n"
+                cd ${SERVER_FOLDER}
+                make
+                cd - > /dev/null
+                echo
+                echo
+            fi
+            sleep 1
+            if [ -f ${SERVER_FOLDER}/server ] ; then
+                echo -e "Start server..."
+                ${SERVER_FOLDER}/server
+            fi
+            exit
+            ;;
+        -rebuildserver)
+            echo -e "Building server...\n"
+            cd ./resource/server
+            make clean
+            make
+            sleep 1
+            cd - > /dev/null
+            if [ -f ${SERVER_EXECUTABLE} ] ; then
+                echo -e "\nStart server..."
+                ${SERVER_EXECUTABLE}
+            fi
+            exit
+            ;;
+        *)
+            ;;
+    esac
+fi
 
-if test "$START_SERVER" == "y" ; then
-    echo -e "\nStart server..."
-    ./server
-    cd ../..
-fi 
+echo -e "usage:"
+echo -e "\tautorun.sh                 : copy html folder to apache2 html folder"
+echo -e "\tautorun.sh -createdb       : create database"
+echo -e "\tautorun.sh -startserver    : start server"
+echo -e "\tautorun.sh -rebuildserver  : rebuild and start server"
