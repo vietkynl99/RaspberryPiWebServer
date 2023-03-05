88418924cc6c142bc42ab7f848d5e1c81b6ec34f
vietkynl99 <vietdohoang1999@gmail.com>
2023-03-05 09:43:33 +0700

    Add autocomple config for input element

--- html/login/LoginPage.php
+++ html/login/LoginPage.php
@@ -47,7 +47,7 @@ session_start();
 
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="pass" placeholder="Type your password">
+						<input class="input100" type="password" name="pass" placeholder="Type your password" autocomplete="on">
 						<span class="focus-input100" data-symbol="&#xf190;"></span>
 					</div>
 					
