e8f0e69e0713f982e6f49dea2feedf3582080eaa
vietkynl99 <vietdohoang1999@gmail.com>
2023-03-18 21:45:53 +0700

    Edit createDatabase.js

--- database/createDatabase.js
+++ database/createDatabase.js
@@ -7,19 +7,22 @@ const fs = require('fs')
 fs.readFile('kynlweb.sql', (err, data) => {
     if (err) throw err;
 
-    let queryArray = data.toString().split(';');
+    // remove space and comment
+    let text = data.toString().replace(/[\s]*[--]+.+[\r\n]+/gm, "").trim();
+    // remove last ;
+    text = text.substring(0, text.length -1)
+    let queryArray = text.split(';');
 
-    for (let i = 0; i <= queryArray.length - 2; i++) {
-        let query = queryArray[i].trim() + ';';
-        // console.log((i + 1 ) + '. ' + query);
-
+    for (let i = 0; i < queryArray.length; i++) {
+        let query = queryArray[i].trim();
+        // console.log((i + 1) + '. ' + query);
         sqlAdapter.query(query, function (success, result) {
             if (success == false) {
-                console.log('[ERROR] SQL query error: ' + query);
+                console.log('[ERROR] SQL query error: index=' + (i+1) + ' query=' + query);
                 process.exit()
             }
-            if (i === queryArray.length - 2) {
-                console.log('\nDone!');
+            if (i === queryArray.length - 1) {
+                console.log('\nExecute ' + queryArray.length + ' queries successfully!!');
                 process.exit()
             }
         })
