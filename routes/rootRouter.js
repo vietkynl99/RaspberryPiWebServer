55de54caaa562ebd700ffcd378b502881dd7d937
vietkynl99 <vietdohoang1999@gmail.com>
2023-03-23 20:00:28 +0700

    Remove unnecessary functions.

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -1,35 +1,10 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var cookieParser = require('cookie-parser');
-
-app.use(cookieParser());
-
-// connect to database
-var sqlAdapter = require('../modules/sqlAdapter')
-sqlAdapter.connect()
 
 // Http request
 router.get('/', function (req, res) {
-	let username = sqlAdapter.removeSpecialCharacter(req.cookies.username);
-	let token = sqlAdapter.removeSpecialCharacter(req.cookies.token);
-	if (!username || !token) {
-		res.redirect('/login');
-		return;
-	}
-	// check token
-	sqlAdapter.query(`SELECT username FROM userinfo WHERE username='${username}' AND token='${token}' AND lastlogin >= DATE_SUB(NOW(), INTERVAL 1 HOUR)`,
-		function (success, result) {
-			if (success == false) {
-				res.redirect('/login');
-			}
-			else if (result.length <= 0) {
-				res.redirect('/login');
-			}
-			else {
-				res.redirect('/home');
-			}
-		})
+	res.redirect('/home');
 });
 
 module.exports = router;
