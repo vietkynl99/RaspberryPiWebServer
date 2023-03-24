05594367b52ee3d47ba96b3338585c710bdca021
vietkynl99 <vietdohoang1999@gmail.com>
2023-03-24 22:41:16 +0700

    Change the table structure of database

--- database/kynlweb.sql
+++ database/kynlweb.sql
@@ -3,20 +3,20 @@ CREATE DATABASE IF NOT EXISTS `kynlwebdb`;
 DROP TABLE IF EXISTS `kynlwebdb`.`userinfo`;
 
 CREATE TABLE `kynlwebdb`.`userinfo` (
-  `username` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(50) NOT NULL,
-  `name` VARCHAR(50) NOT NULL,
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname` VARCHAR(50) NOT NULL,
   `email` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
   `phone` VARCHAR(15) NOT NULL,
+  `birthday` DATE NOT NULL,
   `permission` INT NOT NULL,
   `token` VARCHAR(50),
   `lastlogin` DATETIME,
-  PRIMARY KEY (`username`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
+  PRIMARY KEY (`email`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
 
-INSERT INTO `kynlwebdb`.`userinfo` (`username`, `password`, `name`, `email`, `phone`, `permission`) VALUES ('vietkynl', '123456', 'Viet Kynl', 'vietkynl123@gmail.com', '0123456789', '1');
-INSERT INTO `kynlwebdb`.`userinfo` (`username`, `password`, `name`, `email`, `phone`, `permission`) VALUES ('admin', '123456', 'Admin', 'admin@gmail.com', '0123456', '1');
-INSERT INTO `kynlwebdb`.`userinfo` (`username`, `password`, `name`, `email`, `phone`, `permission`) VALUES ('testuser', '123456', 'Nguyen Van An', 'nguyevanan001@gmail.com', '0123456789', '2');
+INSERT INTO `kynlwebdb`.`userinfo` (`firstname`, `lastname`, `email`, `password`, `phone`, `birthday`, `permission`) VALUES ('Viet', 'Kynl', 'vietkynl@gmail.com', '123456', '0869333444', '1999-01-01', '1');
+INSERT INTO `kynlwebdb`.`userinfo` (`firstname`, `lastname`, `email`, `password`, `phone`, `birthday`, `permission`) VALUES ('Admin', 'System', 'admin@gmail.com', '123456', '0869123123', '1999-01-01', '1');
 
 DROP TABLE IF EXISTS `kynlwebdb`.`loginhistory`;
 
