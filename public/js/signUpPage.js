1d7cbe978c35306b6c3ebc4417bfcdd9901538d6
vietkynl99 <107388318+vietkynl99@users.noreply.github.com>
2023-03-24 17:58:42 +0700

    Add validate for sign up form

--- public/js/signUpPage.js
+++ public/js/signUpPage.js
@@ -20,22 +20,45 @@ $('.validate-form .input100').each(function () {
     });
 });
 
-var input = $('.validate-input .input100');
+var inputValidate = $('.validate-input .input100');
 $('.validate-form').on('submit', function (e) {
     e.preventDefault();
     var check = true;
-    for (var i = 0; i < input.length; i++) {
-        if (validate(input[i]) == false) {
-            showValidate(input[i]);
+    for (var i = 0; i < inputValidate.length; i++) {
+        if (validate(inputValidate[i]) == false) {
+            showValidate(inputValidate[i]);
             check = false;
         }
     }
 });
 
 function validate(input) {
-    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-        if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    if ($(input).attr('name') == 'name') {
+        if ($(input).val().trim().match(/^([a-zA-Z0-9 ]+)$/) == null) {
             return false;
+        }
+    }
+    else if ($(input).attr('name') == 'email') {
+        if ($(input).val().trim().match(/^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/) == null) {
+            return false;
+        }
+    }
+    else if ($(input).attr('name') == 'phone') {
+        if ($(input).val().trim().match(/^(\+84|0)\d{9}$/) == null) {
+            return false;
+        }
+    }
+    else if ($(input).attr('name') == 'pass' || $(input).attr('name') == 'confirm-pass') {
+        if ($(input).val().trim().match(/^([a-zA-Z0-9!-~ ]+)$/) == null) {
+            return false;
+        }
+        if ($(input).val().trim().match(/(['"]+)/) != null) {
+            return false;
+        }
+        if ($(input).attr('name') == 'confirm-pass') {
+            if ($(inputValidate[inputValidate.length - 2]).val().trim() != $(inputValidate[inputValidate.length - 1]).val().trim()) {
+                return false;
+            }
         }
     }
     else {
@@ -43,6 +66,7 @@ function validate(input) {
             return false;
         }
     }
+    return true;
 }
 
 function showValidate(input) {
