50e22f858535451cc8d11ba6baa1f9a5e257e503
vietkynl99 <107388318+vietkynl99@users.noreply.github.com>
2023-03-28 17:07:20 +0700

    Disable port select when port is connected

--- public/js/connectivity.js
+++ public/js/connectivity.js
@@ -14,18 +14,15 @@
             PortList.forEach(element => {
                 html += `<option value="${element.path}">${element.path} ${element.description}</option>`;
             });
-            document.getElementById('portSelect').innerHTML = html;
+            document.getElementById('port-select').innerHTML = html;
         }
 
         function updatePortStatus(status) {
-            if (status === true) {
-                document.getElementById('port-status').innerHTML = `<p>Status: <span class="text-success">Connected</span></p>`;
-
-            }
-            else {
-                document.getElementById('port-status').innerHTML = `<p>Status: <span class="text-danger">Disconnected</span></p>`;
-
-            }
+            portStatus = status === true;
+            document.getElementById('port-status').innerHTML = (status === true) ? `<p>Status: <span class="text-success">Connected</span></p>` : `<p>Status: <span class="text-danger">Disconnected</span></p>`;
+            document.getElementById('button-connect').textContent = (status === true) ? 'Disconnect' : 'Connect';
+            document.getElementById('button-connect').disabled = false;
+            document.getElementById('port-select').disabled = portStatus === true;
         }
         // ===============================================================================================
         socket.on('connect', function () {
@@ -34,27 +31,22 @@
         });
 
         socket.on('user info', function (data) {
-            document.getElementById('navbar_fullname').textContent = data.name;
+            if(data) {
+                document.getElementById('navbar_fullname').textContent = data.name;
+            }
         });
 
         socket.on('update portlist', function (data) {
-            updatePortList(data);
-            $("#icon-refesh").removeClass("fast-spin");
+            if(data) {
+                updatePortList(data);
+                $("#icon-refesh").removeClass("fast-spin");
+            }
         });
 
         socket.on('port status', function (data) {
             console.log('get port status', data);
-            if (data.status === true) {
-                portStatus = true;
-                document.getElementById('port-status').innerHTML = `<p>Status: <span class="text-success">Connected</span></p>`;
-                document.getElementById('button-connect').textContent = 'Disconnect';
-                document.getElementById('button-connect').disabled = false;
-            }
-            else {
-                portStatus = false;
-                document.getElementById('port-status').innerHTML = `<p>Status: <span class="text-danger">Disconnected</span></p>`;
-                document.getElementById('button-connect').textContent = 'Connect';
-                document.getElementById('button-connect').disabled = false;
+            if(data) {
+                updatePortStatus(data.status);
             }
         });
 
@@ -68,12 +60,12 @@
             else {
                 button.textContent = 'Connecting...';
                 button.disabled = true;
-                socket.emit('req connect', { email: email, port: $('#portSelect').val() });
+                socket.emit('req connect', { email: email, port: $('#port-select').val() });
             }
         });
 
         $("#icon-refesh").click(function () {
-            document.getElementById('portSelect').innerHTML = '';
+            document.getElementById('port-select').innerHTML = '';
             $("#icon-refesh").addClass("fast-spin");
             socket.emit('req portlist', email);
         });
